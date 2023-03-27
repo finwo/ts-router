e8927418a650c116feba09e8fa74e75be6e12b67
Yersa Nordman <yersa@finwo.nl>
2023-03-28 00:28:20 +0200

    Add script to version-bump everything

---
+++ scripts/version.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+
+# Fetch the current version
+VERSION=$(jq -r .version < ../package.json)
+
+# Echo only mode
+if [ -z "$1" ]; then
+  echo ${VERSION}
+  exit 0
+fi
+
+# Deconstruct the version number
+MAJOR=$(echo ${VERSION} | cut -d. -f1)
+MINOR=$(echo ${VERSION} | cut -d. -f2)
+PATCH=$(echo ${VERSION} | cut -d. -f3)
+
+# Bump the version accordingly
+case "$1" in
+  major)
+    VERSION="$((MAJOR+1)).0.0"
+    ;;
+  minor)
+    VERSION="$((MAJOR)).$((MINOR+1)).0"
+    ;;
+  patch)
+    VERSION="$((MAJOR)).$((MINOR)).$((PATCH+1))"
+    ;;
+  *)
+    echo "Unknown type: $1" >&1
+    exit 1
+    ;;
+esac
+
+# And save again
+jq ".version = \"${VERSION}\"" < ../package.json | sponge ../package.json
+npm run version:sync
+echo ${VERSION}
