ffbe9ec7bafc08fa934b23a6f0e72cf4dcd013a6
Yersa Nordman <yersa@finwo.nl>
2023-03-27 23:10:25 +0200

    Remove compiled files from repo

--- packages/router-fastify/lib/cjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-export { Service, Inject } from '@finwo/di';
-export { FastifyReply as Response, FastifyRequest as Request };
-export * from './decorators';
-export declare function plugin(router: FastifyInstance, controllers: Function[], done: Function): void;
-export default plugin;
-//# sourceMappingURL=index.d.ts.map
